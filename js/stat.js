fb0b6181d21f686fc76080b38712a3cd5c067d86
mikestoun3 <mikestoun3@gmail.com>
2017-01-29 15:29:37 +0700

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,15 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillRect(100, 70, 420, 210);
+  ctx.fillRect(100, 30, 420, 270);
   ctx.fillStyle = 'white';
 
-  ctx.fillRect(90, 60, 420, 210);
+  ctx.fillRect(90, 20, 420, 270);
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7))';
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
-  ctx.fillText('Ура вы победили!', 110, 90);
-  ctx.fillText('Список результатов:', 110, 110);
+  ctx.fillText('Ура вы победили!', 110, 50);
+  ctx.fillText('Список результатов:', 110, 70);
 
   var max = -1;
 
@@ -21,7 +21,7 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   var histogramHeight = 150;
-  var histogramX = 140;
+  var histogramX = 150;
   var step = histogramHeight / max;
   var columnIndent = 90;
 
@@ -36,9 +36,9 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       ctx.fillStyle = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
     }
-    ctx.fillRect(histogramX + columnIndent * i, 220, 40, -height);
+    ctx.fillRect(histogramX + columnIndent * i, 250, 40, -height);
     ctx.fillStyle = '#000';
     ctx.font = '10px PT Mono';
-    ctx.fillText(name + ':' + time.toFixed(0), histogramX + columnIndent * i, 100 + histogramHeight);
+    ctx.fillText(name + ':' + time.toFixed(0), histogramX + columnIndent * i, 120 + histogramHeight);
   }
 };
