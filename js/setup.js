8a31128c25fba64b7a0f84fa2763195876177c50
mikestoun3 <mikestoun3@gmail.com>
2017-02-08 13:02:08 +0700

    eslint corrected

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 
 //Попап
 var offInvisible = document.querySelector('.setup-open');
-var onInvisible = document.querySelector('.setup-close')
+var onInvisible = document.querySelector('.setup-close');
 var setup = document.querySelector('.setup');
 
 offInvisible.addEventListener('click', function(){
@@ -23,37 +23,40 @@ var wizardCoatColors = [
   'rgb(146, 100, 161)',
   'rgb(56, 159, 117)',
   'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)',
+  'rgb(0, 0, 0)'
 ];
 var wizardEyesColors = [
   'black',
   'red',
   'blue',
   'yellow',
-  'green',
+  'green'
 ];
 var fireballColors = [
   '#ee4830',
   '#30a8ee',
   '#5ce6c0',
   '#e848d5',
-  '#e6e848',
+  '#e6e848'
 ];
 
 
 wizardCoat.addEventListener('click', function() {
-  var colorNum =   Math.floor(Math.random()* wizardCoatColors.length);
+  var colorNum;
+  colorNum = Math.floor(Math.random() * wizardCoatColors.length);
   wizardCoat.style.fill = wizardCoatColors[colorNum];
 });
 
 
 wizardEyes.addEventListener('click', function() {
-  var colorNum =   Math.floor(Math.random()* wizardEyesColors.length);
+  var colorNum;
+  colorNum = Math.floor(Math.random() * wizardEyesColors.length);
   wizardEyes.style.fill = wizardCoatColors[colorNum];
 });
 
 fireballColor.addEventListener('click', function() {
-  var colorNum =   Math.floor(Math.random()* fireballColors.length);
+  var colorNum;
+  colorNum = Math.floor(Math.random() * fireballColors.length);
   fireballColor.style.background = fireballColors[colorNum];
 });
 
