d4572bd4343666a3b378f716cf9efc945d65c39b
mikestoun3 <mikestoun3@gmail.com>
2017-02-24 04:45:13 +0700

    Один обработчик для всех фильтров

--- js/form.js
+++ js/form.js
@@ -4,7 +4,7 @@ var uploadPhoto = document.querySelector('#upload-file');
 var overlay = document.querySelector('.upload-overlay');
 var uploadButton = document.querySelector('#upload-select-image');
 var cancelOverlay = document.querySelector('.upload-form-cancel');
-var filtersPreview = overlay.querySelector('.upload-form-preview');
+var filtersPreview = overlay.querySelector('.filter-image-preview');
 var originalFilter = overlay.querySelector('.upload-filter-label');
 var chromeFilter = overlay.querySelector('.upload-filter-label-chrome');
 var sepiaFilter = overlay.querySelector('.upload-filter-label-sepia');
@@ -14,7 +14,9 @@ var heatFilter = overlay.querySelector('.upload-filter-label-heat');
 var previewSizeDec = overlay.querySelector('.upload-resize-controls-button-dec');
 var previewSizeInc = overlay.querySelector('.upload-resize-controls-button-inc');
 var resizeValue = overlay.querySelector('.upload-resize-controls-value');
+var filtersGroup = overlay.querySelector('.upload-filter-controls');
 var resizePercent = 100;
+
 
 uploadPhoto.addEventListener('change', function () {
   overlay.classList.remove('invisible');
@@ -27,24 +29,6 @@ cancelOverlay.addEventListener('click', function () {
 });
 
 
-originalFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview';
-});
-chromeFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview filter-chrome';
-});
-sepiaFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview filter-sepia';
-});
-marvinFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview filter-marvin';
-});
-phobosFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview filter-phobos';
-});
-heatFilter.addEventListener('click', function () {
-  filtersPreview.className = 'filter-image-preview filter-heat';
-});
 
 previewSizeDec.addEventListener('click', setDecValue);
 previewSizeInc.addEventListener('click', setIncValue);
@@ -70,3 +54,28 @@ function setIncValue() {
 function resizeImage() {
   filtersPreview.style.transform = 'scale(' + resizePercent / 100 + ')';
 }
+
+
+var toggleFilterAriaPressed = function () {
+  var inputs = document.getElementsByName('upload-filter');
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].setAttribute('aria-pressed', inputs[i].checked);
+  }
+};
+var toggleAriaHidden = function (element) {
+  if (element.getAttribute('aria-hidden') === 'true') {
+    element.setAttribute('aria-hidden', false);
+  } else {
+    element.setAttribute('aria-hidden', true);
+  }
+};
+filtersGroup.addEventListener('click', function () {
+  var target = event.target;
+  if (target.tagName.toLowerCase() !== 'input') {
+    return;
+  } else {
+    filtersPreview.className = 'filter-image-preview';
+    filtersPreview.classList.add('filter-' + target.value);
+  }
+  toggleFilterAriaPressed();
+}, false);
