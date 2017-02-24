77ad36db67d531459d25e92fdfff679e41edaf0d
mikestoun3 <mikestoun3@gmail.com>
2017-02-25 06:46:09 +0700

    modulated

--- js/form.js
+++ js/form.js
@@ -4,11 +4,6 @@ var uploadButton = document.querySelector('#upload-select-image');
 var uploadFile = uploadButton.querySelector('#upload-file');
 var Overlay = document.querySelector('.upload-overlay');
 var uploadFormCancel = document.querySelector('.upload-form-cancel');
-var previewSizeDec = document.querySelector('.upload-resize-controls-button-dec');
-var previewSizeIn = document.querySelector('.upload-resize-controls-button-inc');
-var controlValue = document.querySelector('.upload-resize-controls-value');
-var preview = document.querySelector('.filter-image-preview');
-var uploadFilterControls = document.querySelector('.upload-filter-controls');
 
 var ENTER_KEY_CODE = 13;
 var ESCAPE_KEY_CODE = 27;
@@ -63,69 +58,5 @@ uploadFormCancel.addEventListener('click', function () {
   closeUploadOverlayElement();
 });
 
-uploadFilterControls.addEventListener('click', function () {
-  var target = event.target;
-  if (target.tagName.toLowerCase() !== 'input') {
-    return;
-  } else {
-    preview.className = 'filter-image-preview';
-    preview.classList.add('filter-' + target.value);
-  }
-  toggleFilterAriaPressed();
-}, false);
-
-uploadFilterControls.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    if (event.target.tagName.toLowerCase() === 'label') {
-      preview.className = 'filter-image-preview';
-      var labelFor = event.target.getAttribute('for');
-      var input = document.getElementById(labelFor);
-      input.checked = true;
-      preview.classList.add('filter-' + input.value);
-      toggleFilterAriaPressed();
-    }
-  }
-}, true);
 
 
-var decValue = function (valueControl, min, n) {
-  if (valueControl > min) {
-    return (valueControl - n);
-  } else {
-    return valueControl;
-  }
-};
-
-var incValue = function (valueControl, max, n) {
-  if (valueControl < max) {
-    return (valueControl + n);
-  } else {
-    return valueControl;
-  }
-};
-
-previewSizeDec.addEventListener('click', function () {
-  var value = decValue(parseInt(controlValue.value, 10), 25, 25);
-  if (value === 25) {
-    previewSizeDec.disabled = true;
-    previewSizeIn.disabled = false;
-  } else {
-    previewSizeDec.disabled = false;
-    previewSizeIn.disabled = false;
-  }
-  controlValue.value = value + '%';
-  preview.style.transform = 'scale(' + value / 100 + ')';
-});
-
-previewSizeIn.addEventListener('click', function () {
-  var value = incValue(parseInt(controlValue.value, 10), 100, 25);
-  if (value === 100) {
-    previewSizeIn.disabled = true;
-    previewSizeDec.disabled = false;
-  } else {
-    previewSizeDec.disabled = false;
-    previewSizeIn.disabled = false;
-  }
-  controlValue.value = value + '%';
-  preview.style.transform = 'scale(' + value / 100 + ')';
-});
---
+++ js/initialize-filters.js
@@ -0,0 +1,33 @@
+'use strict';
+
+window.initializeFilters = (function () {
+  var preview = document.querySelector('.filter-image-preview');
+  var uploadFilterControls = document.querySelector('.upload-filter-controls');
+
+  return function () {
+
+    uploadFilterControls.addEventListener('click', function () {
+      var target = event.target;
+      if (target.tagName.toLowerCase() !== 'input') {
+        return;
+      } else {
+        preview.className = 'filter-image-preview';
+        preview.classList.add('filter-' + target.value);
+      }
+      toggleFilterAriaPressed();
+    }, false);
+
+    uploadFilterControls.addEventListener('keydown', function (evt) {
+      if (isActivateEvent(evt)) {
+        if (event.target.tagName.toLowerCase() === 'label') {
+          preview.className = 'filter-image-preview';
+          var labelFor = event.target.getAttribute('for');
+          var input = document.getElementById(labelFor);
+          input.checked = true;
+          preview.classList.add('filter-' + input.value);
+          toggleFilterAriaPressed();
+        }
+      }
+    }, true);
+  };
+})();
