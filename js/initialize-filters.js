62c3aedabbb7016b013e769392aaedb613ab9a21
mikestoun3 <mikestoun3@gmail.com>
2017-02-26 20:40:45 +0700

    master updating

---
+++ js/initialize-filters.js
@@ -0,0 +1,36 @@
+'use strict';
+
+window.initializeFilters = (function () {
+  var uploadFilterControls = document.querySelector('.upload-filter-controls');
+
+  return function (callback) {
+    uploadFilterControls.addEventListener('keydown', function (evt) {
+      if (window.utils.isActivateEvent(evt)) {
+        if (evt.target.tagName.toLowerCase() === 'label') {
+          var labelFor = evt.target.getAttribute('for');
+          var input = document.getElementById(labelFor);
+          input.checked = true;
+          window.utils.toggleFilterAriaPressed();
+          if (typeof callback === 'function') {
+            callback(input.value);
+          }
+        }
+      }
+    }, true);
+
+    uploadFilterControls.addEventListener('click', function (event) {
+      var target = event.target;
+      if (target.tagName.toLowerCase() !== 'input') {
+        return;
+      }
+
+      window.utils.toggleFilterAriaPressed();
+
+      if (typeof callback === 'function') {
+        callback(target.value);
+      }
+    }, false);
+
+  };
+
+})();
---
+++ js/initialize-scale.js
@@ -0,0 +1,54 @@
+'use strict';
+
+window.initializeScale = (function () {
+  var previewSizeDec = document.querySelector('.upload-resize-controls-button-dec');
+  var previewSizeInc = document.querySelector('.upload-resize-controls-button-inc');
+  var controls = document.querySelector('.upload-resize-controls');
+
+  return function (controlValue, valueScale, step, callback) {
+    var incValue = function (valueControl, max) {
+      if (valueControl < max) {
+        return (valueControl + step);
+      } else {
+        return valueControl;
+      }
+    };
+
+    var decValue = function (valueControl, min) {
+      if (valueControl > min) {
+        return (valueControl - step);
+      } else {
+        return valueControl;
+      }
+    };
+
+    controls.addEventListener('click', function (evt) {
+      if (evt.target === previewSizeDec) {
+        valueScale = decValue(parseInt(controlValue.value, 10), 25);
+        if (valueScale === 25) {
+          previewSizeDec.disabled = true;
+          previewSizeInc.disabled = false;
+        } else {
+          previewSizeDec.disabled = false;
+          previewSizeInc.disabled = false;
+        }
+      }
+
+      if (evt.target === previewSizeInc) {
+        valueScale = incValue(parseInt(controlValue.value, 10), 100);
+        if (valueScale === 100) {
+          previewSizeInc.disabled = true;
+          previewSizeDec.disabled = false;
+        } else {
+          previewSizeDec.disabled = false;
+          previewSizeInc.disabled = false;
+        }
+      }
+      if (typeof callback === 'function') {
+        callback(valueScale);
+
+      }
+    }, false);
+  };
+
+})();
